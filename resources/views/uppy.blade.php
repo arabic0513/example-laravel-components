64ed4d86337240b967db5034ccc5db195f7db3e7
arabic0513 <rustamyusupov230@gmail.com>
2023-10-04 14:32:44 +0500

    uppy

--- resources/views/uppy.blade.php
+++ resources/views/uppy.blade.php
@@ -1,5 +1,16 @@
-@extends('home')
-@section('center_content')
+@extends('adminlte::page')
+
+@section('title', 'Example App')
+@section('content_header')
+@stop
+@section('content')
+    @bukScripts(true)
     <div class="d-flex justify-content-center" id="file"></div>
     <x-SmartsUppy url="{{route('uploadImage')}}" target="#file" fieldName="file"></x-SmartsUppy>
-@endsection
+@stop
+@section('css')
+    @bukStyles(true)
+@stop
+
+@section('js')
+@stop
